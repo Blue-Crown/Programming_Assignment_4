aa2d655040fa92373306fd16fabb368d9b4d961a
Kaylyn Gillespie <kaylyngillespie@Kaylyns-MacBook-Pro.local>
2021-12-13 15:57:54 -0800

    Fixed typos and finished code with notations

--- best.R
+++ best.R
@@ -1,28 +1,36 @@
 ##Finding the best hospital in the state
 
-best <- funtion(state,outcome) {
+best <- function(state,outcome) 
+{
   ##read outcome data:
   file <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   
   ##check that state and outcome data are valid
-  if !any(state == file$State){
+  if (!any(state == file$State)){
     stop("invalid state") }
   ##print "invalid state" if the state input doesn't match any State listed in 
   ##the source file
-  if ((outcome %in% c(heart attack, heart failure, pneumonia)) == FALSE)
+  if ((outcome %in% c("heart attack", "heart failure", "pneumonia")) == FALSE) {
     stop("invalid outcome") }
   ##print "invalid outcome" if the outcome input doesn't match any condition
   ##listed in the listed vector
-  
-  ##return hospital name with lowest 30-day death rate
-  state_subset <- subset(file, State == state)
-    ##subsetting the data frame to the specified state
-    ##specifying column based on outcome AKA medical condition entered:
+
+  state_subset <- subset(file, State == state) ##subset the data frame to the 
+  ##specified state
+
+  ##specify column number based on outcome (i.e. medical condition) entered:
   if (outcome == "heart attack") { 
-    colnum <- 11 }
+    columnNo <- 11 }
   if (outcome == "heart failure") {
-    colnum <- 17 }
-  else {colnum <- 23 }
-  }
-  }
+    columnNo <- 17 }
+  else (columnNo <- 23)
+
+##print hospital name associated with minimum deaths in specified state and 
+##outcome:
+min_row <- which(as.numeric(state_subset[ ,columnNo]) == 
+                   min(as.numeric(state_subset[ ,columnNo]),na.rm = TRUE))
+                 ##lists the row with min deaths, skipping NA values
+                 hospital_names <- state_subset[min_row,2] ##list of hospital names with min deaths
+                 hospitals <- sort(hospital_names) ##sorting hospitals alphabetically
+                 print(hospitals[1]) ##return the name of the first hospital in the list
 }
